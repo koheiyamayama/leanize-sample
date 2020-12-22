a6e6602ffc244f7c3764d29b02b37ec8e4b96ed2
koheiyamayama <kohei.ccw.23@gmail.com>
2020-12-23 00:15:21 +0900

    amplify init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -21,5 +22,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
